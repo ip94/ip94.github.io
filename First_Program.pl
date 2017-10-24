d7bd8881b6e607555683b4ce60209c89d6fa4f1f
ip94 <ip94@users.noreply.github.com>
2017-10-24 01:53:51 -0400

    Initiated first Perl

---
+++ First_Program.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+use CGI':standard';
+use strict;
+print "Content-type: text/html\n\n";
+
+$title = "first Perl program";
+$font = "Sofia";
+print header();
+print body();
+
+sub header {
+$doc_font = imp_fonts($font);
+$all_style = style();
+return qq{<HTML>\n<HEAD>\n<title>$title</title>\n$doc_font\n$all_style\n</head>};
+}
+
+sub imp_fonts {
+return qq{<link href="https://fonts.googleapis.com/css?family=$font" rel="stylesheet">};
+}
+
+sub style {
+$style = qq{
+<style type="text/css"
+<!-- 
+H1 {font-family: '$font'; font-size: 3em; color: red; text-align: center}
+-->
+</style>};
+
+return $style;
+}
+
+sub body {
+
+$body = qq{
+<BODY>
+<H1>This is my $title</H1>
+</BODY>
+</HTML>};
+
+return $body;
+}
